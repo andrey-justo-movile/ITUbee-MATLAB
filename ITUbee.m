1f271882adda9ea8ff636871183deb0f46702cc4
andrey-justo-movile <andrey.justo@movile.com>
2016-12-09 18:41:45 -0200

    using new tests

--- ITUbee.m
+++ ITUbee.m
@@ -8,10 +8,11 @@ source('ITUbee_lib.m');
 %%%%
 %%  Example of test vectors
 %%%%
+% First test
 MSG='01000000000000000000';
 Key='00000000000000000080';
 
-begintime=cputime;
+tic
 %%%%%
 %%%  Chiffrement
 %%%%%
@@ -20,6 +21,57 @@ c=ITUbee_enc(MSG, Key)
 %%%%
 %%  Dechiffrement
 %%%%
-M=ITUbee_dec('761b8299b3f6a99f0838', Key)
+M=ITUbee_dec(c, Key)
 
-cputime-begintime
+toc
+
+% 2 test
+MSG='00000000000000000000';
+Key='00000000000000000000';
+
+tic
+%%%%%
+%%%  Chiffrement
+%%%%%
+c=ITUbee_enc(MSG, Key)
+
+%%%%
+%%  Dechiffrement
+%%%%
+M=ITUbee_dec(c, Key)
+
+toc
+
+% 3 test
+MSG='6925278951fbf3b25ccc';
+Key='c538bd9289822be43363';
+
+tic
+%%%%%
+%%%  Chiffrement
+%%%%%
+c=ITUbee_enc(MSG, Key)
+
+%%%%
+%%  Dechiffrement
+%%%%
+M=ITUbee_dec(c, Key)
+
+toc
+
+% 4 test
+MSG='6925288A51fCf3b25ccc';
+Key='6925288A51fCf3b25ccc';
+
+tic
+%%%%%
+%%%  Chiffrement
+%%%%%
+c=ITUbee_enc(MSG, Key)
+
+%%%%
+%%  Dechiffrement
+%%%%
+M=ITUbee_dec(c, Key)
+
+toc
